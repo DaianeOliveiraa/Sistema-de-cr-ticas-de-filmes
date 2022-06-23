73e3a5dca270c7e55823a8097065bd0980f52c30
DaianeOliveiraa <daianeldeoliveira@gmail.com>
2022-06-23 17:40:26 -0300

    Projeto criado

---
+++ src/main/java/br/com/daiane/desafioletscode/DesafioLetsCodeApplication.java
@@ -0,0 +1,13 @@
+package br.com.daiane.desafioletscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLetsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLetsCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/daiane/desafioletscode/DesafioLetsCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daiane.desafioletscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLetsCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
